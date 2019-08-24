8a53c6b18c5ac3fd5399842736be20b91485813b
AusCoder <sebmuellermath@gmail.com>
2019-08-24 18:05:49 +1000

    feat(vm_translator): start function, call return

--- compiler/vm_translator.py
+++ compiler/vm_translator.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 import itertools
 import logging
+from collections import deque
 from enum import Enum
 from pathlib import Path
 from typing import Optional, Tuple, List, Iterable
@@ -132,12 +133,30 @@ class Parser:
 
 
 class CodeGenerator:
-    def __init__(self, static_prefix: str) -> None:
-        self.static_prefix = static_prefix
-        self._jmp_count = 0
+    def __init__(self, verbose: bool = True) -> None:
+        self.static_prefix = None
+        self._uniq_int = 0
+        self._fn_call_stack = deque()
+        self._verbose = verbose
+
+    def current_fn_name(self):
+        try:
+            return self._fn_call_stack[-1]
+        except IndexError:
+            return "Sys.init"
+
+    def generate_init(self) -> str:
+        lines = ["// bootstrap"] if self._verbose else []
+        lines.extend([
+            "@256",
+            "D=A",
+            "@SP",
+            "M=D",
+        ])
+        return "\n".join(lines) + "\n" + self.generate_call("Sys.init", 0)
 
     def generate_arithmetic(self, argument: str) -> str:
-        lines = [f"// {argument}"]
+        lines = [f"// {argument}"] if self._verbose else []
         if argument in ["add", "sub", "and", "or"]:
             lines = self._generate_binary_op(lines, argument)
         elif argument in ["eq", "gt", "lt"]:
@@ -178,21 +197,21 @@ class CodeGenerator:
                 "@SP  // D=RAM[*SP - 1] - D",
                 "A=M-1",
                 "D=M-D",
-                f"@eq.true{self._jmp_count}",
+                f"@{argument}.true{self._uniq_int}",
                 jump_line,
                 "@SP  // RAM[*SP - 1]=false (ie 0)",
                 "A=M-1",
                 "M=0",
-                f"@eq.end{self._jmp_count}",
+                f"@{argument}.end{self._uniq_int}",
                 "0; JMP",
-                f"(eq.true{self._jmp_count})  // RAM[*SP - 1]=true (ie -1)",
-                "@SP",
+                f"({argument}.true{self._uniq_int})",
+                "@SP  // RAM[*SP - 1]=true (ie -1)",
                 "A=M-1",
                 "M=-1",
-                f"(eq.end{self._jmp_count})",
+                f"({argument}.end{self._uniq_int})",
             ]
         )
-        self._jmp_count += 1
+        self._uniq_int += 1
         return lines
 
     def _generate_unary_op(self, lines: List[str], argument: str) -> List[str]:
@@ -206,7 +225,10 @@ class CodeGenerator:
         return lines
 
     def generate_push(self, memory_segment: str, argument: int) -> str:
-        lines = [f"// push {memory_segment} {argument}"]
+        if self.static_prefix is None:
+            raise RuntimeError("static_prefix not set")
+
+        lines = [f"// push {memory_segment} {argument}"] if self._verbose else []
         if memory_segment in ["local", "argument", "this", "that"]:
             translated_segment = self._translate_memory_segment(memory_segment)
             lines.extend(
@@ -223,8 +245,7 @@ class CodeGenerator:
             lines.extend([f"@{self.static_prefix}.{argument}", "D=M"])
             lines.extend(self._push_d_sp_inc())
         elif memory_segment == "constant":
-            lines.extend([f"@{argument}  // D={argument}", "D=A"])
-            lines.extend(self._push_d_sp_inc())
+            lines.extend(self._push_constant(argument))
         elif memory_segment == "pointer":
             if 0 > argument > 1:
                 raise InvalidArgumentError(f"pointer argument: {argument}")
@@ -245,8 +266,19 @@ class CodeGenerator:
         """Sets RAM[*SP]=D and SP++"""
         return ["@SP  // RAM[*SP]=D", "A=M", "M=D", "@SP  // SP++", "M=M+1"]
 
+    def _push_constant(self, argument: int) -> List[str]:
+        if argument in [0, 1]:
+            lines = [f"D={argument}  // D={argument}"]
+        else:
+            lines = [f"@{argument}  // D={argument}", "D=A"]
+        lines.extend(self._push_d_sp_inc())
+        return lines
+
     def generate_pop(self, memory_segment: str, argument: int) -> str:
-        lines = [f"// pop {memory_segment} {argument}"]
+        if self.static_prefix is None:
+            raise RuntimeError("static_prefix not set")
+
+        lines = [f"// pop {memory_segment} {argument}"] if self._verbose else []
         if memory_segment in ["local", "argument", "this", "that"]:
             translated_segment = self._translate_memory_segment(memory_segment)
             lines.extend(
@@ -282,7 +314,7 @@ class CodeGenerator:
             raise RuntimeError(f"Unexpected memory segment: {memory_segment}")
         return "\n".join(lines)
 
-    def _sp_dec_pop_d(self):
+    def _sp_dec_pop_d(self) -> List[str]:
         """SP-- and D=RAM[*SP]"""
         return ["@SP  // SP-- and D=RAM[*SP]", "AM=M-1", "D=M"]
 
@@ -299,17 +331,116 @@ class CodeGenerator:
             raise RuntimeError(f"Unexpected memory segment: {memory_segment}")
 
     def generate_label(self, symbol: str) -> str:
-        lines = [f"// label {symbol}", f"({symbol})"]
+        label = f"{self.current_fn_name()}${symbol}"
+        lines = [f"// label {label}", f"({label})"] if self._verbose else []
         return "\n".join(lines)
 
     def generate_goto(self, symbol: str) -> str:
-        lines = [f"// goto {symbol}", f"@{symbol}", "0; JMP"]
+        label = f"{self.current_fn_name()}${symbol}"
+        lines = [f"// goto {label}", f"@{label}", "0; JMP"] if self._verbose else []
         return "\n".join(lines)
 
     def generate_if_goto(self, symbol: str) -> str:
-        lines = [f"// if-goto {symbol}"]
+        label = f"{self.current_fn_name()}${symbol}"
+        lines = [f"// if-goto {label}"] if self._verbose else []
         lines.extend(self._sp_dec_pop_d())
-        lines.extend([f"@{symbol}", "D; JNE"])
+        lines.extend([f"@{label}", "D; JNE"])
+        return "\n".join(lines)
+
+    def generate_function(self, name: str, num_locals: int) -> str:
+        self._fn_call_stack.append(name)
+        lines = [f"// function {name} {num_locals}"] if self._verbose else []
+        lines.extend([f"({name})", "D=0"])
+        for _ in range(num_locals):
+            lines.extend(self._push_d_sp_inc())
+        return "\n".join(lines)
+
+    def generate_return(self) -> str:
+        self._fn_call_stack.pop()
+        lines = ["// return"] if self._verbose else []
+        lines.extend([
+            "@LCL  // R13 = LCL = FRAME",
+            "D=M",
+            "@R13",
+            "M=D",
+            "@5  // R14 = LCL-5 = return address",
+            "D=D-A",
+            "@R14",
+            "M=D",
+            "@SP  // *ARG = return value",
+            "A=M-1",
+            "D=M",
+            "@ARG",
+            "A=M",
+            "M=D",
+            "@ARG  // SP = ARG + 1",
+            "D=M",
+            "@SP",
+            "M=D+1",
+            "@R13  // THAT = *(FRAME - 1)",
+            "D=M",
+            "A=D-1",
+            "D=M",
+            "@THAT",
+            "M=D",
+            "@R13  // THIS = *(FRAME - 2)",
+            "D=M",
+            "@2",
+            "A=D-A",
+            "D=M",
+            "@THIS",
+            "M=D",
+            "@R13  // ARG = *(FRAME - 3)",
+            "D=M",
+            "@3",
+            "A=D-A",
+            "D=M",
+            "@ARG",
+            "M=D",
+            "@R13  // LCL = *(FRAME - 4)",
+            "D=M",
+            "@4",
+            "A=D-A",
+            "D=M",
+            "@LCL",
+            "M=D",
+            "@R14  // goto return address",
+            "A=M",
+            "0; JMP",
+        ])
+        return "\n".join(lines)
+
+    def generate_call(self, name: str, num_arguments: int) -> str:
+        lines = [f"// call {name} {num_arguments}"] if self._verbose else []
+        return_label = f"RETURN_{self._uniq_int}"
+        self._uniq_int += 1
+        self._fn_call_stack.append(name)
+        # push return address
+        lines.extend([f"@{return_label}", "D=A"])
+        lines.extend(self._push_d_sp_inc())
+        # push local, argument, this, that onto stack
+        for segment in ["LCL", "ARG", "THIS", "THAT"]:
+            lines.extend([f"@{segment}", "D=M"])
+            lines.extend(self._push_d_sp_inc())
+        # set ARG = SP - num_arguments - 5
+        lines.extend(
+            [
+                f"@{num_arguments} // ARG=SP - num_arguments - 5",
+                "D=A",
+                "@5",
+                "D=D+A",
+                "@SP",
+                "D=M-D",
+                "@ARG",
+                "M=D",
+            ]
+        )
+        # set LCL = SP
+        lines.extend(["@SP  // LCL = SP", "D=M", "@LCL", "M=D"])
+        # goto f
+        lines.extend([f"@{name}  // goto {name}", "0; JMP"])
+        # return address label
+        lines.extend([f"({return_label})  // return address"])
         return "\n".join(lines)
 
 
@@ -329,37 +460,57 @@ def main(args: argparse.Namespace) -> None:
     else:
         output_filepath = input_filepath.parent.joinpath(f"{input_filepath.stem}.asm")
 
+    if input_filepath.is_dir():
+        needs_bootstrap = True
+        input_filepaths = input_filepath.glob("*.vm")
+    else:
+        needs_bootstrap = False
+        input_filepaths = [input_filepath]
+
     logger.info(f"Input: {input_filepath}")
     logger.info(f"Output: {output_filepath}")
 
-    code_gen = CodeGenerator(input_filepath.stem)
-    with open(input_filepath) as fh:
-        with open(output_filepath, "w") as out_fh:
-            p = Parser(fh)
-            while p.has_next_command():
-                command_type = p.advance()
-                if command_type == CommandType.NONE:
-                    continue
-                elif command_type == CommandType.ARITHMETIC:
-                    arg, _ = p.arguments()
-                    code = code_gen.generate_arithmetic(arg)
-                elif command_type == CommandType.PUSH:
-                    code = code_gen.generate_push(*p.arguments())
-                elif command_type == CommandType.POP:
-                    code = code_gen.generate_pop(*p.arguments())
-                elif command_type == CommandType.LABEL:
-                    label, _ = p.arguments()
-                    code = code_gen.generate_label(label)
-                elif command_type == CommandType.GOTO:
-                    label, _ = p.arguments()
-                    code = code_gen.generate_goto(label)
-                elif command_type == CommandType.IFGOTO:
-                    label, _ = p.arguments()
-                    code = code_gen.generate_if_goto(label)
-                else:
-                    raise RuntimeError(f"Unexpected command type: {command_type}")
-                out_fh.write(code)
-                out_fh.write("\n")
+    code_gen = CodeGenerator()
+    with open(output_filepath, "w") as out_fh:
+        if needs_bootstrap:
+            out_fh.write(code_gen.generate_init())
+            out_fh.write("\n")
+        for input_filepath in input_filepaths:
+            code_gen.static_prefix = input_filepath.stem
+            with open(input_filepath) as fh:
+                p = Parser(fh)
+                while p.has_next_command():
+                    command_type = p.advance()
+                    if command_type == CommandType.NONE:
+                        continue
+                    elif command_type == CommandType.ARITHMETIC:
+                        arg, _ = p.arguments()
+                        code = code_gen.generate_arithmetic(arg)
+                    elif command_type == CommandType.PUSH:
+                        code = code_gen.generate_push(*p.arguments())
+                    elif command_type == CommandType.POP:
+                        code = code_gen.generate_pop(*p.arguments())
+                    elif command_type == CommandType.LABEL:
+                        label, _ = p.arguments()
+                        code = code_gen.generate_label(label)
+                    elif command_type == CommandType.GOTO:
+                        label, _ = p.arguments()
+                        code = code_gen.generate_goto(label)
+                    elif command_type == CommandType.IFGOTO:
+                        label, _ = p.arguments()
+                        code = code_gen.generate_if_goto(label)
+                    elif command_type == CommandType.FUNCTION:
+                        name, num_locals = p.arguments()
+                        code = code_gen.generate_function(name, num_locals)
+                    elif command_type == CommandType.CALL:
+                        name, num_args = p.arguments()
+                        code = code_gen.generate_call(name, num_args)
+                    elif command_type == CommandType.RETURN:
+                        code = code_gen.generate_return()
+                    else:
+                        raise RuntimeError(f"Unexpected command type: {command_type}")
+                    out_fh.write(code)
+                    out_fh.write("\n")
 
 
 if __name__ == "__main__":
